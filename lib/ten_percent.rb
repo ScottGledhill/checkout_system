e86520a7fe60a8728b2ae6dd494b20c2cc53a253
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-07-19 21:27:06 +0100

    ten percent logic refactored

--- lib/ten_percent.rb
+++ lib/ten_percent.rb
@@ -3,10 +3,6 @@ require 'checkout'
 class Ten_percent_off
 
   def ten_percent(basket)
-    if basket >= 60
-      basket / 10.to_f * 9
-    else
-      basket
-    end
+    basket >= 60 ? basket / 10.to_f * 9 : basket
   end
 end
